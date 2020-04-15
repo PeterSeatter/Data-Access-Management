1f80f322147a7d44235d264834207d3403c7844a
Charlie Maru <charliemaru@gmail.com>
2020-04-15 13:21:35 +1200

    Update browseArticlesByKeyword.sql
    updated code

--- Data-Access-Management-master v2.0/schoolsystemproject/queries/browseArticlesByKeyword.sql
+++ Data-Access-Management-master v2.0/schoolsystemproject/queries/browseArticlesByKeyword.sql
@@ -1,5 +1,31 @@
 -- -----------------------------------------------------
--- Example, insert keyword between percent signs
+-- Example, Use CALL browseArticlesByKeyword (‘mon’)
 -- -----------------------------------------------------
 
-SELECT * FROM article where name like '%mon%';
+USE `schoolsystemproject`; 
+
+DROP procedure IF EXISTS ` browseArticlesByKeyword `; 
+
+ DELIMITER // 
+
+USE `schoolsystemproject`// 
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `searchTitle`(browseArticlesByKeyword VARCHAR(45)) 
+
+BEGIN 
+
+SELECT category.categoryName, article.articleID, article.name 
+
+FROM category  
+
+JOIN article  
+
+ON category.categoryID = article.Category_categoryID  
+
+WHERE name LIKE CONCAT ( '%', browseArticlesByKeyword, "%") 
+
+ORDER BY category.categoryID ASC; 
+
+ END// 
+
+ DELIMITER ; 
