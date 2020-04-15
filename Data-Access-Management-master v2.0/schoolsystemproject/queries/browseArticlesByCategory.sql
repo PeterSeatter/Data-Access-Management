cd11f4a8254691df6d4fa9943766a1143b18f490
Charlie Maru <charliemaru@gmail.com>
2020-04-15 13:20:19 +1200

    Update browseArticlesByCategory.sql
    updated code

--- Data-Access-Management-master v2.0/schoolsystemproject/queries/browseArticlesByCategory.sql
+++ Data-Access-Management-master v2.0/schoolsystemproject/queries/browseArticlesByCategory.sql
@@ -1,8 +1,25 @@
-SELECT
-	category.categoryID,
-    category.categoryName,
-    article.articleID,
-    article.name
-FROM category
-JOIN article ON category.categoryID = article.Category_categoryID
-ORDER BY category.categoryID ASC
+USE `schoolsystemproject`; 
+
+DROP procedure IF EXISTS `browseArticlesByCategory`; 
+
+DELIMITER // 
+
+USE `schoolsystemproject`// 
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `browseArticlesByCategory`() 
+
+BEGIN 
+
+SELECT category.categoryName, article.articleID, article.name 
+
+FROM category  
+
+JOIN article  
+
+ON category.categoryID = article.Category_categoryID  
+
+ORDER BY category.categoryID ASC; 
+
+END// 
+
+ DELIMITER ; 
